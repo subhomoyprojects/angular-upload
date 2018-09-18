de87c3bbb67b7163345d89d1c96f4c44199546fc
Subhomoy Pal <subhomoy@techexactly.com>
2018-09-18 13:53:17 +0530

    Add new model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,35 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 // end bootstrap
 
+// material
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// end material
+
 import { AppComponent } from './app.component';
 import { Step1Component } from './registration/step1/step1.component';
 import { AppRoutingModule } from './routing/app-routing.module';
 import { SubscriptionComponent } from './registration/subscription/subscription.component';
 import { HeaderComponent } from './registration/header/header.component';
 import { FooterComponent } from './registration/footer/footer.component';
-
+import { BasicdetailsComponent } from './registration/basicdetails/basicdetails.component';
+import { AdddrinksComponent } from './registration/adddrinks/adddrinks.component';
+import { AddmoreinformationComponent } from './registration/addmoreinformation/addmoreinformation.component';
 @NgModule({
   declarations: [
     AppComponent,
     Step1Component,
     SubscriptionComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BasicdetailsComponent,
+    AdddrinksComponent,
+    AddmoreinformationComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { Step1Component }      from '../registration/step1/step1.component';
 import { SubscriptionComponent }      from '../registration/subscription/subscription.component';
+import { BasicdetailsComponent }      from '../registration/basicdetails/basicdetails.component';
+import { AdddrinksComponent }      from '../registration/adddrinks/adddrinks.component';
+import { AddmoreinformationComponent }      from '../registration/addmoreinformation/addmoreinformation.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/registration/step1', pathMatch: 'full' },
   { path: 'registration/step1', component: Step1Component },
-  { path: 'registration/step2/subscription', component: SubscriptionComponent }
+  { path: 'registration/step2/subscription', component: SubscriptionComponent },
+  { path: 'registration/step2/basicdetails', component: BasicdetailsComponent },
+  { path: 'registration/step2/adddrinks', component: AdddrinksComponent },
+  { path: 'registration/step2/addmoreinfo', component: AddmoreinformationComponent }
 ];
 
 @NgModule({
