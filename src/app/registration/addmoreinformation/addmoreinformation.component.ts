2a79f46973138c98e7813e81cdc226ba9dd50043
Subhomoy Pal <subhomoy@techexactly.com>
2018-09-20 10:58:12 +0530

    Add new component

--- src/app/registration/addmoreinformation/addmoreinformation.component.ts
+++ src/app/registration/addmoreinformation/addmoreinformation.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-addmoreinformation',
   templateUrl: './addmoreinformation.component.html',
-  styleUrls: ['./addmoreinformation.component.css']
+  styleUrls: ['./addmoreinformation.component.css'],
+  providers: [NgbModalConfig, NgbModal]
+
 })
 export class AddmoreinformationComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
   }
 
+  ngOnInit(){
+    
+  }
+
+  open(open_big_image) {
+    this.modalService.open(open_big_image);
+  }
 }
+
+
