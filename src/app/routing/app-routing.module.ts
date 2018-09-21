41d712f6d99cf8caa4c30d553e4aace3590eb459
Subhomoy Pal <subhomoy@techexactly.com>
2018-09-21 16:45:48 +0530

    Change structer

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { Step1Component }      from '../registration/step1/step1.component';
-import { SubscriptionComponent }      from '../registration/subscription/subscription.component';
-import { BasicdetailsComponent }      from '../registration/basicdetails/basicdetails.component';
-import { AdddrinksComponent }      from '../registration/adddrinks/adddrinks.component';
-import { AddmoreinformationComponent }      from '../registration/addmoreinformation/addmoreinformation.component';
+import { Step1Component } from '../registration/step1/step1.component';
+import { SubscriptionComponent } from '../registration/subscription/subscription.component';
+import { BasicdetailsComponent } from '../registration/basicdetails/basicdetails.component';
+import { AdddrinksComponent } from '../registration/adddrinks/adddrinks.component';
+import { AddmoreinformationComponent } from '../registration/addmoreinformation/addmoreinformation.component';
+import { AddoffersComponent } from '../registration/addoffers/addoffers.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/registration/step1', pathMatch: 'full' },
@@ -13,7 +14,8 @@ const routes: Routes = [
   { path: 'registration/step2/subscription', component: SubscriptionComponent },
   { path: 'registration/step2/basicdetails', component: BasicdetailsComponent },
   { path: 'registration/step2/adddrinks', component: AdddrinksComponent },
-  { path: 'registration/step2/addmoreinfo', component: AddmoreinformationComponent }
+  { path: 'registration/step2/addmoreinfo', component: AddmoreinformationComponent },
+  { path: 'registration/step2/addoffers', component: AddoffersComponent }
 ];
 
 @NgModule({
