bc90abb81238020f4d3fdb7f871d30ad4a20ee8f
Subhomoy Pal <subhomoy@techexactly.com>
2018-09-24 11:48:32 +0530

    Change Styling

--- src/app/registration/addoffers/addoffers.component.ts
+++ src/app/registration/addoffers/addoffers.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-addoffers',
   templateUrl: './addoffers.component.html',
-  styleUrls: ['./addoffers.component.css']
+  styleUrls: ['./addoffers.component.css'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class AddoffersComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   ngOnInit() {
+  }
+
+  open(open_big_image) {
+    this.modalService.open(open_big_image);
   }
 
 }
